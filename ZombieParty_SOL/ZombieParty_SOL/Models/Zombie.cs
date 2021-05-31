a4ede3a0e2537ffb523f383c2d1cb0e224188378
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-05-31 02:08:15 -0400

    S01 FCT Zombies R
    Ajouter le ctrl Zombie et la View Index. Ajouter Menu de gestion avec bootstrap

---
+++ ZombieParty_SOL/ZombieParty_SOL/Models/Zombie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZombieParty_SOL.Models
+{
+  public class Zombie
+  {
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Point { get; set; }
+
+  }
+}
